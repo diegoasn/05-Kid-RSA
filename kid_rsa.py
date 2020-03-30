921ea5818ecb52b695a6511daab4a3c19b87ec47
Diego Alberto Salinas Navarro <53946829+diegoasn@users.noreply.github.com>
2020-03-30 15:12:47 -0600

    Add files via upload

--- kid_rsa.py
+++ kid_rsa.py
@@ -2,12 +2,25 @@
 
 import fileinput
 
+"""setup_kid_RSA(a, b, A, B)
+   Calcula los elementos M, e, d y n, necesarios para el cifrado y descifrado de kid RSA.
+   a - número entero random
+   b - número entero random
+   A - número entero random
+   B - número entero random""" 
 def setup_kid_RSA(a, b, A, B):
 	M = (a * b) - 1
 	e, d = (A * M) + a, (B * M) + b
 	n = (e * d - 1) // M
 	return [M, e, d, n]
 
+"""kid_RSA(mode, a, b, A, B)
+   mode - 'E' para cifrar y 'D' para descifrar
+   a - número entero random
+   b - número entero random
+   A - número entero random
+   B - número entero random
+   m - mensaje a cifrar/descifrar"""
 def kid_RSA(mode, a, b, A, B, m):
 	s = setup_kid_RSA(int(a), int(b), int(A), int(B))
 	if mode == 'E':
